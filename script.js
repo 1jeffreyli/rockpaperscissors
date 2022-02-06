e95e9ef6f87116cafecd1817b7b05018a9d3f665
Jeffrey Li <lijeffre@grinnell.edu>
2022-02-06 20:24:16 +0900

    Add play functionality and results

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function computerPlay(){
     let computerNumber = Math.floor(Math.random() * 3);
     return moves[computerNumber];
 }
+
+function play(playerSelection, computerSelection){
+    let playerMove = playerSelection.toLowerCase();
+    let computerMove = computerSelection.toLowerCase();
+    if (playerMove === "rock" && computerMove === "paper") {
+        return "You Lose! Paper beats Rock.";
+    } else if (playerMove === "rock" && computerMove === "scissors") {
+        return "You Win! Rock beats Paper.";
+    } else if (playerMove === "paper" && computerMove === "rock") {
+        return "You Win! Paper beats Rock.";
+    } else if (playerMove === "paper" && computerMove === "scissors") {
+        return "You Lose! Scissors beats Paper.";
+    } else if (playerMove === "scissors" && computerMove === "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerMove === "scissors" && computerMove === "paper") {
+        return "You Win! Scissors beats Paper.";
+    } else {
+        return "It's a tie. Play again.";
+    }
+}
