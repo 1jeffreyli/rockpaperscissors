7a86bde996b0b08f364f70a4d2cb92f6553eb88b
Jeffrey Li <lijeffre@grinnell.edu>
2022-02-06 20:30:23 +0900

    Create draft of game function

--- script.js
+++ script.js
@@ -30,3 +30,10 @@ function play(playerSelection, computerSelection){
 const playerSelection = "rock";
 const computerSelection = computerPlay();
 console.log(play(playerSelection, computerSelection));
+
+function game(){
+    for (let i=0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, or Scissors?", "");
+        
+    }
+}
